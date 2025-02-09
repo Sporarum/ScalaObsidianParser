67700780016ac35ca665e45e74f3c21089b4b277
Quentin Bernet <quentin.bernet@bluewin.ch>
2025-02-09 16:03:11 +0100

    Add example parser and prototype test infrastructure

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,33 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import fastparse.{*, given}
+import fastparse.NoWhitespace.noWhitespaceImplicit
+import fastparse.Parsed.Success
+import fastparse.Parsed.Failure
 
-def msg = "I was compiled by Scala 3. :)"
+def parseA[$: P] = P("a")
+
+@main def hello(): Unit =
+  testParse("a", parseA, ())
+
+
+  /*
+  testParse("b", parseA, ())
+  val f @ Parsed.Failure(label, index, extra) = parse("b", parseA(using _)): @unchecked
+  assert(label == "")
+  assert(index == 0)
+  assert(f.msg == """Position 1:1, found "b"""")
+  */
+
+  println("No errors!")
+
+
+def testParser[T](parser: P[?] ?=> P[T])(testCases: (String, T)*) =
+  testCases.foreach{ case (s, expected) =>
+    testParse(s, parser, expected)
+  }
+
+def testParse[T](s: String, parser: P[?] ?=> P[T], expected: T) =
+  parse[T](s, parser(using _)) match
+    case Success(got, index) =>
+      assert(got == expected)
+    case f@Failure(label, index, extra) =>
+      assert(false, f.msg)
